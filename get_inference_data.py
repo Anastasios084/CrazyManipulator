7c042f7d406c5db7ba93ff4e92a0f5212e4473df
Anastasios084 <ta.saras@hotmail.com>
2024-03-04 22:31:06 +0200

    Almost 15k

--- get_inference_data.py
+++ get_inference_data.py
@@ -188,7 +188,7 @@ def get_inference_data(size):
 
 
     file_path = "./inference/inference_data.json"
-    # Now, try to load the JSON from the new file
+    # Now, try to load the JSON from the new filerqwerrqwerrqwerrqwerrqwerrqwerqwerrqwerrqwerrqwerrqwerqwerqwerrqwerrqwerrqwerqwerqwer
     with open(file_path, 'r', encoding='utf-8') as file:
         json_data = json.load(file)  # This should now work without errors
 
@@ -201,7 +201,7 @@ def get_inference_data(size):
         print("Combined jsons removed successfully")
     else:
         print("There are no  combined jsons to remove.")
-
+     
 
     return df
 
